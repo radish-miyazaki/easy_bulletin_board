853c8aeeee8f22af9c3cb968f806f3912185e26a
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2021-08-08 22:25:50 +0900

    create theme list page.

--- boards/urls.py
+++ boards/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'boards'
 
 urlpatterns = [
+    path('list_themes', views.list_themes, name='list_themes'),
     path('create_theme', views.create_theme, name='create_theme'),
 ]
--- boards/views.py
+++ boards/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from . import forms
+from .models import Themes, Comments
 
 
 def create_theme(request):
@@ -11,8 +12,19 @@ def create_theme(request):
 
         create_theme_form.save()
         messages.success(request, '掲示板を作成しました。')
-        return redirect('accounts:home')
+        return redirect('boards:list_themes')
 
     return render(request, 'boards/create_theme.html', context={
         'create_theme_form': create_theme_form,
     })
+
+
+def list_themes(request):
+    themes = Themes.objects.fetch_all_themes()
+    return render(
+        request,
+        'boards/list_themes.html',
+        context={
+            'themes': themes,
+        }
+    )
